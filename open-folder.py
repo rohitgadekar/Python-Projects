3485795ef021295c24660f94a2137c504c7d45db
Rohit <rohitgadekar86@gmail.com>
2022-09-18 16:40:01 +0530

    added passcode login

--- open-folder.py
+++ open-folder.py
@@ -1,3 +1,11 @@
 import os
+from getpass_asterisk.getpass_asterisk import getpass_asterisk
 
-os.startfile(r"D:\MagicianPerf5003325")
+passcode = "021900"  # Specify Passcode here
+
+value = str(getpass_asterisk("Enter Passcode : "))
+if value == passcode:
+    os.startfile(r'D:\MagicianPerf5003325')  # Enter Folder Path in 'here'
+else:
+    print("Access Denied")
+    exit(0)
