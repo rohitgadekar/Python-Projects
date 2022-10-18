60909c647e40ac403fb5fcb5cf87ffe96b90b5cc
Rohit <rohitgadekar86@gmail.com>
2022-10-18 19:56:22 +0530

    updated

--- Calculator/cc.py
+++ Calculator/cc.py
@@ -3,15 +3,17 @@ from ssl import SSLErrorNumber
 from tkinter import *
 from ast import Lambda
 from tokenize import Floatnumber
+from turtle import right
 from numpy import insert
+from tkinter import messagebox
 
 root = Tk()
 root.title('Calculator')
 root.iconbitmap('icon.ico')
 root.wm_resizable(width=0, height=0)
 
-e = Entry(root, width=40, borderwidth=5)
-e.grid(row=0, column=0, columnspan=4, padx=5, pady=2)
+e = Entry(root, width=40, borderwidth=2,justify='right')
+e.grid(row=0, column=0, columnspan=5, padx=10, pady=10)
 
 # Functions
 
@@ -28,84 +30,99 @@ def button_clear():
 
 
 def click_add():
-    fn = e.get()
-    global fnum, math
-    math = 'add'
-    fnum = int(fn)
-    e.delete(0, END)
+    if e.get()== "":
+        messagebox.askokcancel("Error","Enter a number to perform addition")
+    else:
+        fn = e.get()
+        global fnum, math
+        math = 'add'
+        fnum = int(fn)
+        e.delete(0, END)
 
 
 def click_sub():
-    fn = e.get()
-    global fnum, math
-    math = 'sub'
-    fnum = int(fn)
-    e.delete(0, END)
+    if e.get()== "":
+        messagebox.askokcancel("Error","Enter a number to perform substraction")
+    else:
+        fn = e.get()
+        global fnum, math
+        math = 'sub'
+        fnum = int(fn)
+        e.delete(0, END)
 
 
 def click_mult():
-    fn = e.get()
-    global fnum, math
-    math = 'mult'
-    fnum = int(fn)
-    e.delete(0, END)
+    if e.get()== "":
+        messagebox.askokcancel("Error","Enter a number to perform multiplication")
+    else:
+        fn = e.get()
+        global fnum, math
+        math = 'mult'
+        fnum = int(fn)
+        e.delete(0, END)
 
 
 def click_div():
-    fn = e.get()
-    global fnum, math
-    math = 'div'
-    fnum = int(fn)
-    e.delete(0, END)
+    if e.get()== "":
+        messagebox.askokcancel("Error","Enter a number to perform division")
+    else:
+        fn = e.get()
+        global fnum, math
+        math = 'div'
+        fnum = int(fn)
+        e.delete(0, END)
 
 
 def click_equal():
-    sn = e.get()
-    e.delete(0, END)
-    if (math == 'add'):
-        e.insert(0, fnum+int(sn))
-    elif(math == 'sub'):
-        e.insert(0, fnum-int(sn))
-    elif(math == 'mult'):
-        e.insert(0, fnum*int(sn))
-    elif(math == 'div'):
-        e.insert(0, fnum/int(sn))
+    if e.get()== "":
+      messagebox.askokcancel("Error","Peform mathematics first") 
+    else:
+        sn = e.get()
+        e.delete(0, END)
+        if (math == 'add'):
+            e.insert(0, fnum+int(sn))
+        elif(math == 'sub'):
+            e.insert(0, fnum-int(sn))
+        elif(math == 'mult'):
+            e.insert(0, fnum*int(sn))
+        elif(math == 'div'):
+            e.insert(0, fnum/int(sn))
 
 
 # Creating Buttons
 
-button1 = Button(root, text='1', padx=30, pady=10,
+button1 = Button(root, text='1',bg='white', padx=30, pady=10,
                  command=lambda: button_click(1))
-button2 = Button(root, text='2', padx=30, pady=10,
+button2 = Button(root, text='2',bg='silver', padx=30, pady=10,
                  command=lambda: button_click(2))
-button3 = Button(root, text='3', padx=30, pady=10,
+button3 = Button(root, text='3',bg='white', padx=30, pady=10,
                  command=lambda: button_click(3))
-button4 = Button(root, text='4', padx=30, pady=10,
+button4 = Button(root, text='4',bg='white', padx=30, pady=10,
                  command=lambda: button_click(4))
-button5 = Button(root, text='5', padx=30, pady=10,
+button5 = Button(root, text='5',bg='silver', padx=30, pady=10,
                  command=lambda: button_click(5))
-button6 = Button(root, text='6', padx=30, pady=10,
+button6 = Button(root, text='6',bg='white', padx=30, pady=10,
                  command=lambda: button_click(6))
-button7 = Button(root, text='7', padx=30, pady=10,
+button7 = Button(root, text='7',bg='white', padx=30, pady=10,
                  command=lambda: button_click(7))
-button8 = Button(root, text='8', padx=30, pady=10,
+button8 = Button(root, text='8',bg='silver', padx=30, pady=10,
                  command=lambda: button_click(8))
-button9 = Button(root, text='9', padx=30, pady=10,
+button9 = Button(root, text='9',bg='white', padx=30, pady=10,
                  command=lambda: button_click(9))
-button0 = Button(root, text='0', padx=30, pady=10,
+button0 = Button(root, text='0',bg='silver', padx=30, pady=10,
                  command=lambda: button_click(0))
 
-buttonadd = Button(root, text='+', padx=20, pady=10,
+buttonadd = Button(root, text='+',bg='white', padx=20, pady=10,
                    command=click_add)
-buttonclear = Button(root, text='C', padx=20, pady=10,
+buttonclear = Button(root, text='c',bg='white', padx=20, pady=10,
                      command=button_clear)
-buttonequal = Button(root, text='=', padx=20, pady=10,
+buttonequal = Button(root, text='=',bg='white', padx=20, pady=10,
                      command=click_equal)
-buttonsub = Button(root, text='-', padx=20, pady=10,
+buttonsub = Button(root, text='-',bg='white', padx=20, pady=10,
                    command=click_sub)
-buttonmult = Button(root, text='*', padx=30, pady=10,
+buttonmult = Button(root, text='*',bg='white', padx=30, pady=10,
                     command=click_mult)
-buttondiv = Button(root, text='/', padx=30, pady=10,
+buttondiv = Button(root, text='/',bg='white', padx=30, pady=10,
                    command=click_div)
 
 # Griding Buttons
